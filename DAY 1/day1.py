0d39c4a6bafa4a3c4123b9f9b11810b9f815460a
DnyanadaMoharir <88180132+DnyanadaMoharir@users.noreply.github.com>
2021-08-30 22:27:36 +0530

    Add files via upload

---
+++ DAY 1/day1.py
@@ -0,0 +1,121 @@
+# WAP for Add,mul,sub,div
+# Addition
+'''a = input('Enter first number: ')
+b = input('Enter second number: ')
+sum = float(a) + float(b)
+print('The sum of {} and {} is {}'.format(a,b,sum))'''
+
+#Subtraction
+'''a = input('Enter first number: ')
+b = input('Enter second number: ')
+diff = float(a) - float(b)
+print('The difference of {} and {} is {}'.format(a,b,diff))'''
+
+# Multiplication
+'''a = input('Enter first number: ')
+b = input('Enter second number: ')
+product = float(a) * float(b)
+print('The product of {} and {} is {}'.format(a,b,product))'''
+
+# Division
+'''a = input('Enter first number: ')
+b = input('Enter second number: ')
+div = float(a) %  float(b)
+print('The remainder of {} and {} is {}'.format(a,b,div))'''
+
+
+
+
+
+# WAP to calculate Simple intrest
+'''P = float(input("Enter the principal amount : "))
+N = float(input("Enter the number of years : "))
+R = float(input("Enter the rate of interest : "))
+SI = (P * N * R)/100
+print("Simple interest : {}".format(SI))'''   
+
+
+
+
+# WAP for swapping using third variable
+'''a = input('Enter first number a: ')
+b = input('Enter second number b: ')
+c = a
+a = b
+b = c
+c = a
+print('Value of a is:', a)
+print('Value of b is:', b)'''
+
+
+
+
+# WAP for swapping without using third variable
+'''a = input('Enter first number a: ')
+b = input('Enter second number b: ')
+a,b = b,a
+print('Value of a is:', a)
+print('Value of b is:', b)'''
+
+
+
+
+# WAP to calculate gross salary from basic salary(HRA=30% , TA=40% , DA=20%)
+'''name = str(input('Enter name of employee:'))
+basic = float(input('Enter basic salary:'))
+DA = float(basic*0.20)
+HRA = float(basic*0.30)
+PF = float((basic+DA)*0.12)
+TA = float(basic*0.40)
+netpay = float(basic+DA+HRA+PF+TA)
+gross_salary = float(netpay-PF)
+print('gross_salary:',gross_salary)'''
+
+
+
+
+# WAP to find circumference of circle
+'''r = float(input('Enter radius of the circle'))
+cirumference = 2*3.14*r
+print('circumference of the circle is:', cirumference)'''
+
+
+
+
+# WAP to find area of the circle
+'''r = float(input('Enter radius of the circle'))
+area = 2*3.14*r*r
+print('area of the circle is:', area)'''
+
+
+
+
+# WAP to enter height of user in feets and convert it into inches and centimeter
+'''h = float(input('Enter the height in feet:'))
+Height_in_inches = h*12
+Height_in_cm = Height_in_inches*2.54
+print('Height in inches is:',Height_in_inches)
+print('Height in cm is:',Height_in_cm)'''
+
+
+
+
+# WAP  to reverse three numbers
+
+
+# WAP to take centigrade temperature and convert it into farenheight temperature
+'''c = float(input('Enter temperature in centigrade'))
+farenheight = (c*9/5)+32
+print('Temperature in Farenheight is:',farenheight)'''
+
+
+
+
+
+
+
+
+
+
+
+
